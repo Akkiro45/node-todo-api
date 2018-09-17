b31e60526deed03f7ba96d2f6e08e30505e7cf76
Akkiro45 <32303778+Akkiro45@users.noreply.github.com>
2018-09-17 20:41:27 +0530

    Added mlab link

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://akshay:Akkiro@45@ds161062.mlab.com:61062/node-todoappv01' || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
   mongoose
