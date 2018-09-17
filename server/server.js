9d668ea3f70792da45e283e1f0119e8900883637
Akkiro45 <32303778+Akkiro45@users.noreply.github.com>
2018-09-17 18:22:38 +0530

    Added GET /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
@@ -26,6 +27,17 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   });
+});
+
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id;
+  if(!ObjectID.isValid(id)) {
+    return res.status(400).send(id + " is not valid ID.");
+  }
+  Todo.findById(id).then((todo) => {
+    if(!todo) return res.status(404).send(id + " Dose not exist.");
+    res.status(200).send(todo);
+  }).catch((e) => res.status(400).send("ERROR"));
 });
 
 app.listen(3000, () => {
