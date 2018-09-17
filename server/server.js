4df532ca03d89032d54a47b5aca81706f0524f15
Akkiro45 <32303778+Akkiro45@users.noreply.github.com>
2018-09-17 18:53:48 +0530

    added test for GET /todos/id

--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ app.get('/todos/:id', (req, res) => {
   }
   Todo.findById(id).then((todo) => {
     if(!todo) return res.status(404).send(id + " Dose not exist.");
-    res.status(200).send(todo);
+    res.status(200).send({todo});
   }).catch((e) => res.status(400).send("ERROR"));
 });
 
