64dfd6435e96686cba471a90992cf6816278a044
Akkiro45 <32303778+Akkiro45@users.noreply.github.com>
2018-09-23 19:48:30 +0530

    Added DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -54,6 +54,17 @@ UserSchema.methods.generateAuthToken = function() {
   });
 };
 
+UserSchema.methods.removeToken = function(token) {
+  const user = this;
+  return user.update({
+    $pull: {
+      tokens: {
+        token
+      }
+    }
+  });
+};
+
 UserSchema.statics.findByCredentials = function(email, password) {
   const User = this;
   User.findOne({email}).then((user) => {
